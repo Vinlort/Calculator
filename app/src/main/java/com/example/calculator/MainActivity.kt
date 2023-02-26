d34ff691e71e4cfe2b988daf3e7e6ef7aa23da87
Vinlort <vetal.nyzhnyk@gmail.com>
2023-02-26 21:40:57 +0200

    refactor + binding

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,141 +2,128 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.calculator.databinding.ActivityMainBinding
 import com.ezylang.evalex.Expression
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var binding: ActivityMainBinding
+    private val numberStringBuilder = StringBuilder()
+    override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val resultTextView = findViewById<TextView>(R.id.result)
 
-        val pointBtn = findViewById<Button>(R.id.point_button)
-        val equalBtn = findViewById<Button>(R.id.equal_button)
-        val plusBtn = findViewById<Button>(R.id.plus_button)
-        val minusBtn = findViewById<Button>(R.id.minus_button)
-        val multiplyBtn = findViewById<Button>(R.id.mult_button)
-        val divideBtn = findViewById<Button>(R.id.div_button)
-        val clearBtn = findViewById<Button>(R.id.clear_button)
-        val clearCharBtn = findViewById<Button>(R.id.clear_one_char_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val zeroBtn = findViewById<Button>(R.id.zero_button)
-        val oneBtn = findViewById<Button>(R.id.one_button)
-        val twoBtn = findViewById<Button>(R.id.two_button)
-        val threeBtn = findViewById<Button>(R.id.three_button)
-        val fourBtn = findViewById<Button>(R.id.four_button)
-        val fiveBtn = findViewById<Button>(R.id.five_button)
-        val sixBtn = findViewById<Button>(R.id.six_button)
-        val sevenBtn = findViewById<Button>(R.id.seven_button)
-        val eightBtn = findViewById<Button>(R.id.eight_button)
-        val nineBtn = findViewById<Button>(R.id.nine_button)
-
-        val numberStringBuilder = StringBuilder()
-
-        oneBtn.setOnClickListener {
+        setListeners()
+    }
+    private  fun setListeners()= with(binding){
+        oneButton.setOnClickListener {
             numberStringBuilder.append("1")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        twoBtn.setOnClickListener {
+        twoButton.setOnClickListener {
             numberStringBuilder.append("2")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
-        threeBtn.setOnClickListener {
+        threeButton.setOnClickListener {
             numberStringBuilder.append("3")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        fourBtn.setOnClickListener {
+        fourButton.setOnClickListener {
             numberStringBuilder.append("4")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        fiveBtn.setOnClickListener {
+        fiveButton.setOnClickListener {
             numberStringBuilder.append("5")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        sixBtn.setOnClickListener {
+        sixButton.setOnClickListener {
             numberStringBuilder.append("6")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        sevenBtn.setOnClickListener {
+        sevenButton.setOnClickListener {
             numberStringBuilder.append("7")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        eightBtn.setOnClickListener {
+        eightButton.setOnClickListener {
             numberStringBuilder.append("8")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        nineBtn.setOnClickListener {
+        nineButton.setOnClickListener {
             numberStringBuilder.append("9")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        zeroBtn.setOnClickListener {
+        zeroButton.setOnClickListener {
             numberStringBuilder.append("0")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        pointBtn.setOnClickListener {
+        pointButton.setOnClickListener {
             numberStringBuilder.append(".")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        multiplyBtn.setOnClickListener {
+        multiplyButton.setOnClickListener {
             numberStringBuilder.append("*")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
-        plusBtn.setOnClickListener {
+        plusButton.setOnClickListener {
             numberStringBuilder.append("+")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
-        minusBtn.setOnClickListener {
+        minusButton.setOnClickListener {
             numberStringBuilder.append("-")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        divideBtn.setOnClickListener {
+        divideButton.setOnClickListener {
             numberStringBuilder.append("/")
-            resultTextView.text = numberStringBuilder
+            resultField.text = numberStringBuilder
         }
 
-        clearBtn.setOnClickListener {
-            resultTextView.text = "0"
+        clearButton.setOnClickListener {
+            resultField.text = "0"
             numberStringBuilder.clear()
         }
 
-        clearCharBtn.setOnClickListener {
+        clearCharButton.setOnClickListener {
             val lastIndexInString = numberStringBuilder.length-1
             if (lastIndexInString >= 0){
-            numberStringBuilder.deleteCharAt(lastIndexInString)
-            resultTextView.text = numberStringBuilder
+                numberStringBuilder.deleteCharAt(lastIndexInString)
+                resultField.text = numberStringBuilder
             }
         }
 
-        equalBtn.setOnClickListener {
+        equalButton.setOnClickListener {
 
-            try {
-                val stringExpression = numberStringBuilder.toString()
-                val exp = Expression(stringExpression)
-                val expRes = exp.evaluate().numberValue
-                resultTextView.text = expRes.toString()
-
-                numberStringBuilder.clear()
-                numberStringBuilder.append(expRes.toString())
-
-            }catch (t: Throwable){
-                Toast.makeText(this@MainActivity, "Exception: $t", Toast.LENGTH_LONG).show()
-            }
-
+            calculate(resultField)
         }
-
     }
 
+    private fun calculate(resultField: TextView) {
+        try {
+            val stringExpression = numberStringBuilder.toString()
+            val exp = Expression(stringExpression)
+            val expRes = exp.evaluate().numberValue
+            resultField.text = expRes.toString()
+
+            numberStringBuilder.clear()
+            numberStringBuilder.append(expRes.toString())
+
+        } catch (t: Throwable) {
+            Toast.makeText(this@MainActivity, "Exception: $t", Toast.LENGTH_LONG).show()
+        }
+    }
 }
