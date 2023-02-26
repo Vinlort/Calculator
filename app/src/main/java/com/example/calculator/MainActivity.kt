1115161c891b34df80b8958113766a299e08e7c7
Vinlort <vetal.nyzhnyk@gmail.com>
2023-02-26 23:25:53 +0200

    added HistoryActivity

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val numberStringBuilder = StringBuilder()
+
+    private  val historyList = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
 
@@ -107,11 +110,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         equalButton.setOnClickListener {
-
             calculate(resultField)
+            saveToHistory()
+        }
+
+        historyButton.setOnClickListener {
+            //new activity
+            val intent = Intent(this@MainActivity, HistoryActivity::class.java)
+            intent.putExtra("history_list", historyList.toTypedArray())
+            startActivity(intent)
         }
     }
 
+    private fun saveToHistory(){
+        val stringExpression = numberStringBuilder.toString()
+        historyList.add(stringExpression)
+    }
     private fun calculate(resultField: TextView) {
         try {
             val stringExpression = numberStringBuilder.toString()
